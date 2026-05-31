656d2d0991e3f07cdcc4f45e88a03092163c87e5
julianabs14 <juliianabs67@gmail.com>
2026-05-31 14:15:03 -0300

    adição de rota status. backend com Flask, criação de API

---
+++ backend/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def inicio():
+    return jsonify({'mensagem': 'API Technosensor funcionando'})
+
+@app.route('/status')
+def status():
+    dados = {
+        'status': 'EM OPEREÇÃO',
+        'pecas_inspecionadas': 9842,
+        'pecas_aprovadas': 9102,
+        'pecas_rejeitadas': 740,
+        'taxa_rejeiao': 7.53
+    }
+    return jsonify(dados)
+
+if __name__ == '__main__':
+    app.run(debug=True)
